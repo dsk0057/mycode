d62bbefd9ce4173b3b1f701d485afceb7ae082a7
Deepak Kafle <dkafle6141@gmail.com>
2022-09-19 22:13:46 +0000

    completed the challenge...lab13

---
+++ input_challenge/input1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# below is a function containing our code
+def main():
+
+    # pause the program and wait for the user to provide input
+    user_input_name = input("Please enter your name: ")
+
+    user_input_day = input("Please enter the day today: ")
+    
+    # display the input back to the user.
+    print(f"Hello, {user_input_name}! Happy {user_input_day}")
+    
+# this calls main
+main()
+
+
+
+    
