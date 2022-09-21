8493d3de85607f91e3ca4f21fb0ca3029af4196a
Deepak Kafle <dkafle6141@gmail.com>
2022-09-21 19:29:50 +0000

    working on if, elif, else logic

---
+++ iftest2/ipask.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Conditionals - testing if strings test true"""
+
+ipchk = input("Apply an IP address: ") # this line now prompts the user for input
+
+if ipchk:
+    print("Looks like the IP address was set: " + ipchk)
