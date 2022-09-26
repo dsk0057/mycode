ea672457a1f6f58fc554189b9b676b11dfaeff9e
Deepak Kafle <dkafle6141@gmail.com>
2022-09-26 22:24:39 +0000

    Learning about sub classes

---
+++ oop2/cheatdice.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""RZFeeser | Alta3 Research
+   Creating a simple dice program utilizing classes."""
+
+# standard library
+from random import randint
+
+class Player:
+    def __init__(self):
+        self.dice = []
+
+    def roll(self):
+        self.dice = []
+        for i in range(3):
+            self.dice.append(randint(1,6))
+
+    def get_dice(self):
+        return self.dice
+
+# allows user to turn their last roll into a 6
+class Cheat_Swapper(Player):
+    def cheat(self):
+        self.dice[-1] = 6
+
+# allows user to increase all rolls if they were less than a 6
+class Cheat_Loaded_Dice(Player):
+    def cheat(self):
+        i = 0
+        while i < len(self.dice):
+            if self.dice[i] < 6:
+                self.dice[i] += 1
+            i += 1
